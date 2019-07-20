847d519d6074aaeb452ede13f5154437b386ab37
a-zhourihu <rihu.zhou@dhc.com.cn>
2019-07-20 20:36:15 +0800

    订单详情 dev

---
+++ pages/order/orderDetail/index.js
@@ -0,0 +1,34 @@
+
+Page({
+    data: {
+        tabs: [
+            {
+                title: "全部",
+                data:[1,2,3,4,5,6,7,8,9]
+            },
+            {
+                title: "待付款",
+                data:[1,2,3,4,5,6,7,8,9]
+            },
+            {
+                title: "待收获",
+                data:[1,2,3,4,5,6,7,8,9]
+            },
+            {
+                title: "已收货",
+                data:[1,2,3,4,5,6,7,8,9]
+            },
+            {
+                title: "已过期",
+                data:[1,2,3,4,5,6,7,8,9]
+            },
+        ]
+    },
+    onLoad: function () {
+        this.setData({
+            logs: (wx.getStorageSync('logs') || []).map(log => {
+                return util.formatTime(new Date(log))
+            })
+        })
+    }
+})
