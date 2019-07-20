e819fd8b120d37f8b0427f8d8ea67ee4e44abd78
a-zhourihu <rihu.zhou@dhc.com.cn>
2019-07-20 18:05:40 +0800

    我的 dev

---
+++ pages/ucenter/index.js
@@ -0,0 +1,34 @@
+//index.js
+const util = require('../../utils/util.js')
+
+Page({
+    data: {
+        items: [
+            {
+                title: "全部",
+                msg:"你的作品最多19个字",
+                icon:"/images/tab/img_doctor.png",
+                time:"30分钟前"
+            },
+            {
+                title: "全部",
+                msg:"你的作品最多19个字",
+                icon:"/images/tab/img_doctor.png",
+                time:"30分钟前"
+            },
+            {
+                title: "全部",
+                msg:"你的作品最多19个字",
+                icon:"/images/tab/img_doctor.png",
+                time:"30分钟前"
+            },
+        ]
+    },
+    onLoad: function () {
+        this.setData({
+            logs: (wx.getStorageSync('logs') || []).map(log => {
+                return util.formatTime(new Date(log))
+            })
+        })
+    }
+})
