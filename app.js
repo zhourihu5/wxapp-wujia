acc7c009b50c2ff2a8dee842c59934c3e3228757
a-zhourihu <rihu.zhou@dhc.com.cn>
2019-08-24 14:47:10 +0800

    bug fix

--- app.js
+++ app.js
@@ -33,17 +33,6 @@ App({
 
         this.globalData.pixelRatio = e.pixelRatio;
         let custom = wx.getMenuButtonBoundingClientRect()
-        if(!custom){
-            console.log('getMenuButtonBoundingClientRect set to default iphone6')
-            custom={//获取不到就设置为iphone6的
-                bottom: 58,
-                height: 32,
-                left: 278,
-                right: 365,
-                top: 26,
-                width: 87,
-            };
-        }
         console.log('getMenuButtonBoundingClientRect', custom)
         this.globalData.Custom = custom;
         this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
@@ -53,32 +42,55 @@ App({
             const e = wx.getSystemInfoSync()
             console.log('获取设备系统信息success',e)
             this.setSystemInfo(e)
-            wx.setStorage({
-                key: 'SystemInfo',
-                data: e
-            })
+            if(this.globalData.StatusBar&&this.globalData.screenWidth&&this.globalData.windowWidth
+                &&this.globalData.windowHeight&&this.globalData.pixelRatio
+            &&this.globalData.Custom
+            &&this.globalData.Custom.bottom
+            &&this.globalData.CustomBar){
+                wx.setStorage({
+                    key: 'SystemInfo',
+                    data: e
+                })
+            }
+
         } catch (ex) {
             console.log('获取设备系统信息fail',ex)
+
+        }
+        if(this.globalData.StatusBar&&this.globalData.screenWidth&&this.globalData.windowWidth
+            &&this.globalData.windowHeight&&this.globalData.pixelRatio
+            &&this.globalData.Custom
+            &&this.globalData.Custom.bottom
+            &&this.globalData.CustomBar){
+
+        }else {
             var e = wx.getStorageSync('SystemInfo')
             if (e) {
                 console.log('从缓存文件中获取设备系统信息成功')
-            } else {
-                console.log('从缓存文件中获取设备系统信息 failed,set SystemInfo default to iphone6')
-                e = {//实在获取不到就设置默认为iphone6的
-                    screenHeight: 667,
-                    screenWidth: 375,
-                    statusBarHeight: 20,
-                    windowHeight: 619,
-                    windowWidth: 375,
-                    pixelRatio: 2,
-
+                try {
+                    this.setSystemInfo(e)
+                } catch (ex2) {
+                    console.log('setSystemInfo err ',ex2)
                 }
             }
-            try {
-                this.setSystemInfo(e)
-            } catch (ex2) {
-                console.log('setSystemInfo exception ',ex2)
-            }
+        }
+
+        if(!this.globalData.StatusBar){
+            this.globalData.StatusBar=31
+            console.log('设置默认 StatusBar',this.globalData.StatusBar)
+        }
+        if(!this.globalData.Custom|| !this.globalData.Custom.bottom){
+            this.globalData.Custom ={//获取不到就设置默认值
+                bottom: 70,
+                height: 32,
+                left: 287,
+                right: 383,
+                top: 38,
+                width: 96,
+            };
+            this.globalData.CustomBar =this.globalData.Custom.bottom + this.globalData.Custom.top - this.globalData.StatusBar;
+            console.log('设置默认 CustomBar',this.globalData.CustomBar)
+            console.log('设置默认 Custom',this.globalData.Custom)
         }
 
         this.judgeAuth();
