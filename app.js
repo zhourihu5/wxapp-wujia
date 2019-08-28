50d14b58ca31119737a78c7141ed5d45e51c45a0
a-zhourihu <rihu.zhou@dhc.com.cn>
2019-08-28 20:34:54 +0800

    bug fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 App({
-    url:'http://192.168.250.6:8081',
+    // url:'http://192.168.250.6:8081',
     // url:'http://192.168.250.134:8081',
-    // url: 'https://api.home-guard.cn',
+    url: 'https://api.home-guard.cn',
     token: null,
     isAuthorized: false,
     myAddress: null,//我的收获地址
