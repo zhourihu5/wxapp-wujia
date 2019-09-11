1ccfdc13930b18680f6062ad47ec2eca110f13cd
a-zhourihu <rihu.zhou@dhc.com.cn>
2019-09-11 17:35:15 +0800

    bug fix

--- pages/myAdress/index.js
+++ pages/myAdress/index.js
@@ -6,6 +6,7 @@ Page({
         CustomBar: app.globalData.CustomBar,
         apiData: null,
         currentAddr:null,
+        modalName: null,
     },
     showNavigationBarLoading(){
         this.setData({
@@ -52,6 +53,14 @@ Page({
         wx.navigateTo({url: "/pages/addAdress/index"})
     },
     itemClick(e) {
+        console.log('itemClick')
+        if(this.data.modalName){
+            this.setData({
+                modalName:null,
+            })
+            return
+        }
+
         var index = e.currentTarget.dataset.index
         var pages = getCurrentPages();
         var prevPage = pages[pages.length - 2];
@@ -71,5 +80,52 @@ Page({
         console.log(this.data.currentAddr)
         wx.navigateTo({url: "/pages/editAdress/index"})
         console.log('跳转后')
+    },
+
+    // ListTouch触摸开始
+    ListTouchStart(e) {
+        this.setData({
+            ListTouchStart: e.touches[0].pageX
+        })
+    },
+
+    // ListTouch计算方向
+    ListTouchMove(e) {
+        this.setData({
+            ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
+        })
+    },
+
+    // ListTouch计算滚动
+    ListTouchEnd(e) {
+        if (this.data.ListTouchDirection =='left'){
+            this.setData({
+                modalName: e.currentTarget.dataset.target
+            })
+        } else {
+            this.setData({
+                modalName: null
+            })
+        }
+        this.setData({
+            ListTouchDirection: null
+        })
+    },
+
+    delete(e){
+        var that = this
+        var index=e.currentTarget.dataset.index
+        var item= this.data.apiData[index]
+        network.requestPost('/v1/address/deleteAddress', {id:item.id}, function (data) {
+            that.data.apiData=that.data.apiData.filter(function(item1) {
+                     return item1.id != item.id
+                 });
+            that.setData({
+                apiData:that.data.apiData,
+            })
+            that.loadData()
+        }, function (msg) {
+
+        })
     },
 })
