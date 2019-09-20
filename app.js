a6d6912b08360e67ef1e98836367fe6f30adf854
a-zhourihu <rihu.zhou@dhc.com.cn>
2019-09-20 17:13:37 +0800

    bug fix

--- app.js
+++ app.js
@@ -130,13 +130,6 @@ App({
                     wx.getUserInfo({
                         success: function (res) {
                             that.wxUserInfo = res
-                            // var userInfo = res.userInfo
-                            // var nickName = userInfo.nickName
-                            // var avatarUrl = userInfo.avatarUrl
-                            // var gender = userInfo.gender //性别 0：未知、1：男、2：女
-                            // var province = userInfo.province
-                            // var city = userInfo.city
-                            // var country = userInfo.country
                         }
                     })
                 } else {
